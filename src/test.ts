2fe47ef9ff556799f1bda9562d88362f6f6ee13f
juanmiguel431 <juanmiguel431@gmail.com>
2023-08-18 11:47:57 -0400

    Mobx - Defining actions.

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,4 @@
-import { makeObservable, observable } from 'mobx';
+import { action, autorun, makeObservable, observable } from 'mobx';
 
 console.log('it works');
 
@@ -7,7 +7,7 @@ const person = observable({
   lastName: 'Paulino Carpio'
 });
 
-console.log('Our person is', person);
+// console.log('Our person is', person);
 
 class Person {
   public firstName: string;
@@ -21,11 +21,24 @@ class Person {
     this.firstName = name;
     this.lastName = lastName;
   }
+
+  public updateFirstName = action((name: string) => {
+    this.firstName = name;
+  })
 }
 
-const newPerson = new Person('Luis Miguel', 'Paulino');
 
-console.log('Class base person', newPerson);
+async function task() {
+  newPerson = new Person('Luis Miguel', 'Paulino');
+  await new Promise(r => setTimeout(r, 3000));
+  newPerson.updateFirstName('Lucas');
+}
 
+let newPerson: Person;
+task();
+
+autorun(() => {
+  console.log('Person name is ' + newPerson.firstName);
+})
 
 export {};
